61b8d473971bddd5e12c006fbcd62799c054b48d
chitanda-xiaolu <183138896@qq.com>
2022-05-26 18:51:13 +0800

    update

---
+++ src/layout/components/sidebar/Link.vue
@@ -0,0 +1,52 @@
+<template>
+  <component :is="type">
+    <slot />
+  </component>
+</template>
+
+<script>
+import { isExternal } from '@/utils/validate'
+
+export default {
+  props: {
+    to: {
+      type: String,
+      require: true
+    }
+  },
+  computed: {
+    isExternal () {
+      return isExternal(this.to)
+    },
+    type () {
+      if (this.isExternal) {
+        return 'a'
+      }
+      return 'router-link'
+    }
+  },
+  methods: {
+    linkProps (to) {
+      if (this.isExternal) {
+        return {
+          href: to,
+          target: '_blank',
+          rel: 'noopener'
+        }
+      }
+      return {
+        to: to
+      }
+    }
+  }
+}
+</script>
+/**
+<a> 标签定义超链接，用于从一张页面链接到另一张页面
+也即使说，通过a标签进行跳转，页面会被重新渲染，即相当于重新打开一个新的网页
+用href属性指定目标地址，target="_blank"打开新窗口
+
+<router-link>具有路由功能的应用中(点击)导航。
+通过 to 属性指定目标地址，默认渲染成带有正确链接的 <a> 标签，可以通过配置 tag 属性生成别的标签.。
+通过router-link进行跳转不会跳转到新的页面，也不会重新渲染，它会选择路由所指的组件进行渲染，避免了重复渲染.
+ */
---
+++ src/layout/components/sidebar/Logo.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="sidebar-logo-container" :class="{'collapse': collapse}">
+    <transition name="sidebarLogonFade">
+      <router-link v-if="collapse" class="sidebar-logo-link" to="/">
+        <img v-if="logo" :src="logo" class="sidebar-logo">
+        <h1 v-else class="sidebar-title">{{ title }}</h1>
+      </router-link>
+      <router-link v-else key="expand" class="sidebar-logo-link" to="/">
+        <img v-if="logo" :src="logo" class="sidebar-logo">
+        <h1 class="sidebar-title">{{ title }}</h1>
+      </router-link>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SidebarLogo',
+  props: {
+    collapse: {
+      type: Boolean,
+      require: true
+    }
+  },
+  data () {
+    return {
+      title: 'Vue Admin Template',
+      logo: 'https://wpimg.wallstcn.com/69a1c46c-eb1c-4b46-8bd4-e9e686ef5251.png'
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
