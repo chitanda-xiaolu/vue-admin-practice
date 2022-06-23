00e8fbfb32cf1c959ebe380d1517c1662e0c6f6b
chitanda-xiaolu <183138896@qq.com>
2022-06-23 18:26:09 +0800

    update

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -30,7 +30,39 @@ const mutations = {
 
 const actions = {
   // user logo
-  getInfo ({ commit, state }) {}
+  login ({ commit }, userinfo) {
+    const { username, password } = userinfo
+    return Promise((resolve, reject) => {
+      login({ username: username.trim(), password: password }).then(response => {
+        const { data } = response
+        commit('SET_TOKEN', data.token)
+        setToken(data.token)
+        resolve()
+      }).catch(error => {
+        reject(error)
+      })
+    })
+  }
+
+  // get user info
+  getInfo ({ commit, state }) {
+    return new Promise((resolve, reject) => {
+      getInfo(state.token).then(response => {
+        const { data } = response
+
+        if (!data) {
+          return reject('Verification failed, please Login again.')
+          const { name, avatar } = data
+
+          commit('SET_NAME', name)
+          commit('SET_AVATAR', avatar)
+          resolve(data)
+        }
+      }).catch(error => {
+        reject(error)
+      })
+    })
+  }
 }
 
 export default {
