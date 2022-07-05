9ddd04bf3d21ece579aa4385e074c32a7ea07826
chitanda-xiaolu <183138896@qq.com>
2022-07-05 17:56:05 +0800

    layout component

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -1,8 +1,22 @@
 <template>
   <div class="navbar">
-    Navbar
     <hamburger :is-active="sidebar.opened" class="hamburger-container" @toggleClick="toggleSideBar" />
-    <breadcrumb class="breadcrumb-container" /> -->
+    <breadcrumb class="breadcrumb-container" />
+    <div class="right-menu">
+      <el-dropdown class="avatar-container" trigger="click">
+        <div class="avatar-wrapper">
+          <img :src="avatar+'?imageView2/1/w/80/h/80'" class="user-avatar">
+          <i class="el-icon-caret-bottom"/>
+        </div>
+        <el-dropdown-menu slot="dropdown" class="user-dropdown">
+          <router-link to="/">
+            <el-dropdown-item>
+              Home
+            </el-dropdown-item>
+          </router-link>
+        </el-dropdown-menu>
+      </el-dropdown>
+    </div>
   </div>
 </template>
 
