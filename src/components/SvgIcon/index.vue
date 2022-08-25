37b73f446a611cb2a39d3e415a5b359bcddfb482
chitanda-xiaolu <183138896@qq.com>
2022-08-25 23:40:43 +0800

    svg-icone

---
+++ src/components/SvgIcon/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div v-if="isExternal"/>
+</template>
+
+<script>
+import { isExternal } from 'util/validate';
+
+export default {
+  name: 'SvgIcon',
+  props: {
+    iconClass: {
+      type: String,
+      required: true
+    },
+    classNmae: {
+      type: String,
+      default: ''
+    }
+  },
+  computed: {
+    isExternal () {
+      return isExternal(this.iconClass)
+    },
+    iconName () {
+      return `#icon-${this.iconClass}`
+    },
+    svgClass () {
+      if (this.className) {
+        return 'svg-icon ' + this.className
+      } else {
+        return 'svg-icon'
+      }
+    },
+    styleExternalIcon () {
+      return {
+        mask: `url(${this.iconClass}) no-repeat 50% 50%`,
+        '-webkit-mask': `url(${this.iconClass}) no-repeat 50% 50%`
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
