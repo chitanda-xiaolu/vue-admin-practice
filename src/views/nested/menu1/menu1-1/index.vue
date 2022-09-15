6e4e58058d363d699a0a7347a25eef4b3d0c1fab
chitanda-xiaolu <183138896@qq.com>
2022-09-15 18:12:11 +0800

    update demo

--- src/views/nested/menu1/menu1-1/index.vue
+++ src/views/nested/menu1/menu1-1/index.vue
@@ -1,13 +1,35 @@
 <template>
-  <div>menu1-1</div>
+  <div>
+    <input type="text" v-model="name">
+    <p>{{ name }}</p>
+    <input type="text" v-model="info.hobby">
+    <p>{{ info }}</p>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      name: 'chitanda',
+      info: {
+        hobby: 'photography'
+      }
+    }
+  },
+  watch: {
+    name (newVal, oldVal) {
+      console.log('name', newVal, oldVal)
+    },
+    info: {
+      handler (newVal, oldVal) {
+        console.log('info', newVal, oldVal)
+      },
+      deep: true
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
